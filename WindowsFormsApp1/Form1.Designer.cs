0f3e703c5ff4d296771b10dc01c41e08358af983
mantaXDD <s187705@student.pg.edu.pl>
2022-10-25 14:00:17 +0200

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(372, 138);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 216);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Untitled_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Jazda po mieście";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer CloudsTimer;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] cloud;
+        Image playerImg;
+        Random rnd;
+        int backgroundspeed;
+        string lastkeypressed;
+        private int currFrame = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            playerImg = new Bitmap("D:\\VISUAL PROJECTS\\WindowsFormsApp1\\WindowsFormsApp1\\Resources\\models.png");
+            pictureBox1.Image = playerImg;
+            timer2.Interval = 10;
+            timer1.Interval = 500;
+            timer2.Tick += new EventHandler(update);
+            timer1.Tick += new EventHandler(update);
+            //pictureBox1.Location = new Point(512,216);
+            this.KeyDown+=new KeyEventHandler(keyboard);
+            
+        }
+
+        private void keyboard(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.W || e.KeyCode ==Keys.Up)
+            {
+                currFrame = 0;
+                //if( lastkeypressed== "A")
+                //{
+                //    currFrame = 7;
+                //}
+                //else if (lastkeypressed == "D")
+                //{
+                //    currFrame = 1;
+                //}
+            }
+            else if(e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                currFrame = 6;
+            }
+            else if(e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                currFrame = 4;
+            }
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                currFrame = 2;
+            }
+            //else if((int)e.KeyData == (int)Keys.Control + (int)Keys.Up)
+            //{
+            //    currFrame = 7;
+            //    MessageBox.Show("Ctrl + Up pressed...");
+            //}
+            //else if(e.Modifiers==Keys.W && e.KeyCode == Keys.D)
+            //{
+            //    currFrame = 1;
+            //}
+            //else if(e.Modifiers==Keys.A && e.KeyCode == Keys.S)
+            //{
+            //    currFrame = 5;
+            //}
+            //else if(e.Modifiers==Keys.D && e.KeyCode == Keys.S)
+            //{
+            //    currFrame = 3;
+            //}
+            label1.Text = "Last: " + e.KeyCode+lastkeypressed;
+            lastkeypressed = e.KeyCode.ToString();
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            playAnimation();
+
+            //currFrame++;
+            if (currFrame == 8)
+            {
+                currFrame = 0;
+            }
+        }
+        private void playAnimation()
+        {
+            if (currFrame != -1)
+            {
+                Image part = new Bitmap(216, 216);
+                Graphics g = Graphics.FromImage(part);
+                g.PageScale = 0.5f;
+                g.ResetTransform();
+                g.DrawImage(playerImg, 0, 0, new Rectangle(new Point(25 + 216 * currFrame, 25), new Size(216, 216)), GraphicsUnit.Pixel);
+                var test = ResizeImage(part, 100, 100);
+                pictureBox1.Image = test;
+            }
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i =0; i < cloud.Length; i++)
+            {
+                cloud[i].Left += backgroundspeed;
+                if (cloud[i].Left >= 1280)
+                {
+                    cloud[i].Left = cloud[i].Height;
+                }
+            }
+            for (int i = cloud.Length; i < cloud.Length; i++)
+            {
+                cloud[i].Left += backgroundspeed - 10;
+                if (cloud[i].Left >= 1280)
+                {
+                    cloud[i].Left = cloud[i].Left;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundspeed = 5;
+            cloud = new PictureBox[20];
+            rnd = new Random();
+            
+            for (int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i] = new PictureBox();
+                cloud[i].BorderStyle = BorderStyle.None;
+                cloud[i].Location = new Point(rnd.Next(-1000, 1280), rnd.Next(140, 320));
+
+                if (i % 2 == 1)
+                {
+                    cloud[i].Size = new Size(rnd.Next(100, 255), rnd.Next(30, 70));
+                    cloud[i].BackColor = Color.FromArgb(rnd.Next(50, 125), 255, 200, 200);
+  
+                }
+                else
+                {
+                    cloud[i].Size = new Size(150, 25);
+                    cloud[i].BackColor = Color.FromArgb(rnd.Next(50, 125), 255, 205, 205);
+                }
+                Controls.Add(cloud[i]);
+
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
